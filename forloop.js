a0e1711664f268a2832979961c8bd5d5f5c79aaf
Harish <arish4703@gmail.com>
2025-03-26 16:13:44 +0530

    initial project

---
+++ forloop.js
@@ -0,0 +1,19 @@
+let n=27
+
+for(let i=0;i<n;i++){
+   
+   let str=""
+   for(let j=0;j<i;j++){
+   
+    str+=String.fromCharCode(65+j);
+    
+   }
+    
+    console.log(str);
+}
+
+let alpha=["a","b","c","d","e"]
+
+for(let i=0;i<=alpha.length;i++){
+    console.log(alpha.slice(0,i).join(","));
+}
---
+++ operators.js
@@ -0,0 +1,19 @@
+let a = 10 , b = 20;
+console.log(a + b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a % b);
+console.log(a ** b);
+
+function addition(d,e,){
+ let f=d+e
+return f;
+}
+console.log(addition(2,3));
+
+function multiplication(mul1,mul2){
+    let mul3=mul1*mul2;
+    return mul3;
+}
+console.log(multiplication(2,3));
