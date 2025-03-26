1ca521c714735fc73bc6d8e0a8ec0cdeeedf78d7
Harish <arish4703@gmail.com>
2025-03-27 00:37:00 +0530

    Updation in for loop

--- forloop.js
+++ forloop.js
@@ -1,3 +1,4 @@
+//for loop
 let n=27
 
 for(let i=0;i<n;i++){
@@ -17,4 +18,37 @@ let alpha=["a","b","c","d","e"]
 
 for(let i=0;i<=alpha.length;i++){
     console.log(alpha.slice(0,i).join(","));
+}
+
+//while loop
+let persons = 1;
+
+while(persons <=5){
+    console.log("persons can seat in a car = member",persons);
+    persons++;
+}
+
+//do while loop
+let number = 1
+do{
+    console.log("Sequence number is",number)
+    number++;
+}while(number <=5){
+    console.log("Your assigned car number is NO",number);
+    
+}
+
+
+// for in loop
+let person = { name: "John", age: 25, city: "New York" };
+
+for (let i in person) {
+    console.log(i + ":", person[i]);
+}
+
+//for of loop
+let cars =["Bolero ZLX","XUV 300","swift VDI","Volvo XC 90"]
+
+for(car of cars){
+    console.log("My favourite vehicles are",car);
 }
