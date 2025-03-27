a4fabcc967f107ea36205ff19c81ab1eebaf6d1d
Harish <arish4703@gmail.com>
2025-03-27 17:17:36 +0530

    Array methods updation

--- arraymethods.js
+++ arraymethods.js
@@ -15,8 +15,32 @@ console.log(numbers);
 let sequence = [9,8,7,6,5,4,3,2,1]
 sequence.splice(1,6);
 console.log(sequence);
+console.log(sequence.splice(9,0,'10'))
+console.log(sequence);
 
 //sliced value based on index was displayed as output
 let marks = [40,50,30,20,60,90,80]
-console.log(marks.slice(1,4));
+console.log(marks.slice(1,4))
+
+//joining the array(Displaying only the array indexes)
+let collections1 = ["Champer","MAXX","Dost"]
+let collection2 = ["Xuv 300","Boler0","Alto"]
+
+let joinedcollection = [collections1,collection2]
+console.log(joinedcollection);
+
+//joining and displaying the values in the array
+JoinedArr = collections1.concat(collection2)
+console.log(JoinedArr)
+
+let joined =[...collections1,...collection2]
+console.log(joined);
+
+
+
+
+
+
+
+
 
